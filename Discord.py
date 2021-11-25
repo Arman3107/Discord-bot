8de3a317845839c9465c10b87bd1fe91955625c3
Armando Calderon <93158352+Arman3107@users.noreply.github.com>
2021-11-25 13:11:52 -0600

    Create Discord.py

---
+++ Discord.py
@@ -0,0 +1,16 @@
+import discord
+from discord.ext import commands
+from discord import FFmpegPCMAudio
+import Musica
+
+cogs = [Musica]
+
+intents = discord.Intents().all()
+client = commands.Bot(command_prefix="!", intents = discord.Intents.all())
+
+for i in range(len(cogs)):
+    cogs[i].setup(client)
+
+
+
+client.run()
